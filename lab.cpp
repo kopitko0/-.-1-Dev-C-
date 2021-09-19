88c4bfb6b266772a4bbb5265548040d2a790f666
User <User@WIN-98ILA84G2QS>
2021-09-20 00:52:06 +0300

    Добавьте файлы проекта.

---
+++ lab.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std; 
+
+int main() {
+    float a, b, S, P;
+    cin >> a;
+    cin >> b;
+    S = a + b;
+    cout << "a + b =" << S;
+    cin >> a;
+    cin >> b;
+    P = 2 * (a + b);
+    cout << "2 * ( a + b )" << P;
+    return 0;
+
+}
